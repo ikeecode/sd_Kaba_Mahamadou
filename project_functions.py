a8ee0cea4f8987315e55825dbb307c29bd894589
ikeecode <ikeecode@gmail.com>
2022-02-24 16:08:27 +0000

    correction dans le fichier project_functions.py

--- project_functions.py
+++ project_functions.py
@@ -7,6 +7,7 @@ from yaml import safe_load
 from csv import DictReader as csv_load
 from csv import DictWriter as csv_write
 from xmltodict import parse
+from dicttoxml import dicttoxml as dtx
 
 def python_structure(file):
     reader = ''
@@ -16,12 +17,16 @@ def python_structure(file):
         elif file.endswith('.yaml') or file.endswith('.yml'):
             reader = safe_load(thefile)
         elif file.endswith('.csv'):
-            this = {}
+            # this = {}
+            this = []
             k = 0
             reader = csv_load(thefile)
+            # for element in reader:
+            #     this.setdefault("value" + str(k), element)
+            #     k +=1
+            # reader = this
             for element in reader:
-                this.setdefault("value" + str(k), element)
-                k +=1
+                this.append(element)
             reader = this
         elif file.endswith('.xml'):
             reader = loads(dumps(parse(thefile.read())))
@@ -29,18 +34,20 @@ def python_structure(file):
 
 
 def converter(pdata, extent):
-    with open('converti.' + extent, 'w') as target:
+    with open('new_file.' + extent, 'w') as target:
         if extent == 'json':
             jdump(dumps(pdata), target)
         elif extent == 'yaml' or extent == 'yml':
             ydump(pdata, target)
         elif extent == 'csv':
-            n = int(input("Entrer la taille de l'entete: "))
-            fieldnames = [input() for i in range(n)]
+            pdata = [pdata]
+            fieldnames = pdata[0].keys()
+            # n = int(input("Entrer la taille de l'entete: "))
+            # fieldnames = [input() for i in range(n)]
             output = csv_write(target, fieldnames)
             output.writeheader()
             for elem in pdata:
-                output.writerow(pdata[elem])
+                output.writerow(elem)
         elif extent == 'xml':
-            # use dictoxml
-            print("bii mom khamagouma")
+            xml = dtx(pdata)
+            target.write(str(xml, 'utf-8'))
